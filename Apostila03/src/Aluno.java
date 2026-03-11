16e167fbcfb54dfe60ee15d962e2e5a87cdda406
Yutose5256 <y.a.arakaki200*@gmail.com>
2026-03-11 20:50:36 -0300

    Projeto da apostila 03

---
+++ Apostila03/src/Aluno.java
@@ -0,0 +1,17 @@
+public class Aluno {
+
+    public static void main(String[] args) {
+
+        //definir os atributos do aluno
+        short idade;
+        String nomeCurso;
+        boolean maticulado;
+        double valorMensalidade;
+        char turma;
+        int id;
+        String nome;
+        
+
+
+    }
+}
---
+++ Apostila03/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bom dia");
+
+            //declarar variaveis no java
+            short idade;
+            //atribuir um valor para a variavel 
+            idade = 32;
+            //declarar a variavel e atribuir um valor
+            int anoNascimento = 1994;
+            double salario = 16000.50;
+            float altura = 16000.50f;
+            char inicialNome = 'L';
+            boolean maiorIdade = true; //false
+            String nome = "Lucas";
+    }
+}

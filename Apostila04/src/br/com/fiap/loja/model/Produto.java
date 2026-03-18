81a7b0e94832cfe0ad069d3b444e193dd9e4a28c
Yuto <y.a.arakaki2008@gmail.com>
2026-03-18 20:55:22 -0300

    Add files via upload

---
+++ Apostila04/src/br/com/fiap/loja/model/Produto.java
@@ -0,0 +1,12 @@
+package br.com.fiap.loja.model;
+
+public class Produto {
+
+    public static void main(String[] args) {
+        //ler os dados do produto
+        //criar o objeto produto
+        //Atribuir os valores aao objeto
+        //exibir os valores dos atributos
+
+    }
+}
---
+++ Apostila04/src/br/com/fiap/loja/view/Terminal.java
@@ -0,0 +1,11 @@
+package br.com.fiap.loja.view;
+
+public class Terminal {
+
+    double preco;
+    int quantidade;
+    String nome;
+    boolean garantia;
+
+
+}
